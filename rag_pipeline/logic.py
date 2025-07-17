d619dceac7abe09e9692e38cb2ccaaa0283837c5
CHENCHAIAH MEKALATHURU <137814794+iamchenchu@users.noreply.github.com>
2025-07-16 20:33:40 -0500

    Added code

--- rag_pipeline/logic.py
+++ rag_pipeline/logic.py
@@ -1,23 +1,18 @@
-from langchain.document_loader import PyPDFLoader
+from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFaceHub
 
-
-
 def run_rag(pdf_path, query):
-    # Load the PDF document 
     loader = PyPDFLoader(pdf_path)
-    pages = loader.load()
+    docs = loader.load()
 
-    # Initialize embeddings and vector store
-    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6_v2")
-    vector_store = Chroma.from_documents(pages, embeddings)
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectorstore = Chroma.from_documents(docs, embeddings)
 
-    retriver = vector_store.as_retriever()
-    llm = HuggingFaceHub(repo_id ="google/flan-t5-base", model_kwargs={"temperature": 0.1, "max_length": 100})
+    retriever = vectorstore.as_retriever()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-base", model_kwargs={"temperature": 0.1, "max_length": 100})
 
-    # Create the RetrievalQA chain
-    qa_chain = RetrievalQA.from_chain_type(llm=llm, retriver=retriver)
-    return qa_chain.run(query)
+    qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    return qa.run(query)
